06808d83538162697b5bb02e20da6d898bc66051
Saul Hernandez <sauldhernandez@gmail.com>
2015-05-13 22:37:23 -0430

    Now properly calls initialize

--- dist/sgb-screen-signup.js
+++ dist/sgb-screen-signup.js
@@ -2,9 +2,10 @@
 'use strict';
 
 angular.module('sgb-screen-signup', ['megazord'])
-  .controller('sgb-screen-signup-controller', ['$stateParams', '_screenParams','$scope', function($stateParams, _screenParams, $scope){
+  .controller('sgb-screen-signup-controller', ['$stateParams', '_screen', '_screenParams','$scope', function($stateParams, _screen, _screenParams, $scope){
 
-        $scope.title = _screenParams.title || 'signup_title';
+        _screen.initialize($scope, _screenParams);
+
         $scope.params = $stateParams.data;
 
         $scope.performSubmit = function(){};
